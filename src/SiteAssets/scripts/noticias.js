a35242d6097f31ca98bb157e1aa1e3de8c7f90f5
ikaroamorim <ikaro.amorim@gmail.com>
2021-11-21 18:06:00 -0300

    Usando o pipe date e o ng-bind

--- src/SiteAssets/scripts/noticias.js
+++ src/SiteAssets/scripts/noticias.js
@@ -1,16 +1,23 @@
 function getItems($scope, listName, query) {
    var data = [];
 
+   var optionDate = { year:"numeric", month:"long", day:"numeric"};
+
    $.ajax({
       url: `/sites/CursoUdemy/_api/lists/getbytitle('${listName}')/items${query}`,
       method: "GET",
-      headers: { 
-         "accept": "application/json;odata=verbose" 
+      headers: {
+         "accept": "application/json;odata=verbose"
       },
       async: false,
       success: function (data) {
-         console.log(data);
+         //console.log(data);
          $scope.items = data.d.results;
+         $scope.formatarData = function(dataPublicacao){
+            var data = new Date(dataPublicacao).toLocaleDateString("pt-BR",optionDate);
+            console.log('retorno data:',data)
+            return data;
+         }
       },
       error: function (sender, args) {
          console.log(args.get_message());
